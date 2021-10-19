daa99ac1c8de5def5aec9003cbdedfe22a1ce709
Dang Tran <dang.tran@employmenthero.com>
2021-10-19 10:49:32 +0700

    update cors

--- app/controllers/map_urls_controller.rb
+++ app/controllers/map_urls_controller.rb
@@ -12,11 +12,21 @@ class MapUrlsController < ActionController::Base
   end
 
   def create
-    origin = request.headers["HTTP_ORIGIN"]
+    origin = request.headers['HTTP_ORIGIN']
     original_url = params.require(:original_url)
     shorten_name = params[:shorten_name]
 
-    if shorten_name
+    if shorten_name.to_s.strip.empty?
+      loop do
+        shorten_url = format(I18n.t('s_s'), origin, Faker::Alphanumeric.alphanumeric(number: 10))
+        next if MapUrl.find_by(shorten_url: shorten_url)
+
+        map_url = MapUrl.create({ shorten_url: shorten_url, original_url: original_url })
+        render json: { data: { shorten_url: map_url.shorten_url } }
+        break
+      end
+
+    else
       user_shorten_url = format(I18n.t('s_s'), origin, shorten_name)
       map_url = MapUrl.find_or_initialize_by(shorten_url: user_shorten_url) do |m|
         m.original_url = original_url
@@ -24,20 +34,10 @@ class MapUrlsController < ActionController::Base
 
       if map_url.new_record?
         map_url.save
-        render json: { error: { message: 'Shorten URL created successfully.' } }
+        render json: { data: { shorten_url: map_url.shorten_url } }
       else
         render json: { error: { message: 'Shorten URL has already been existed.' } }
       end
-    else
-      loop do
-        shorten_url = format(I18n.t('s_s'), origin, Faker::Alphanumeric.alphanumeric(number: 10))
-        unless MapUrl.find_by(shorten_url: shorten_url)
-          MapUrl.create({ shorten_url: shorten_url, original_url: original_url })
-          break
-        end
-      end
-
-      render json: { error: { message: 'Shorten URL created successfully.' } }
     end
   end
 end
